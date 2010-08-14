026f2e49d4bdfc06116d74cd7dc7907f31dcaa15
Brian Rosner <brosner@gmail.com>
2010-08-14 00:22:11 -0600

    made Player.name unique

--- manoria_project/apps/manoria/models.py
+++ manoria_project/apps/manoria/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Player(models.Model):
     
     user = models.ForeignKey(User, related_name="players")
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     
     # @@@ points
 
