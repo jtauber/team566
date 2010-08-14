bd0ffe4076bf7b1de2e3eeeac3bcc91f4094bc8e
Brian Rosner <brosner@gmail.com>
2010-08-14 00:19:39 -0600

    added login_required to views

--- manoria_project/apps/manoria/views.py
+++ manoria_project/apps/manoria/views.py
@@ -2,10 +2,13 @@ from django.http import Http404
 from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response, redirect
 
+from django.contrib.auth.decorators import login_required
+
 from manoria.forms import PlayerCreateForm
 from manoria.models import Player
 
 
+@login_required
 def player_detail(request, name):
     player = get_object_or_404(Player, name=name)
     
@@ -19,6 +22,7 @@ def player_detail(request, name):
     return render_to_response("manoria/player_detail.html", ctx)
 
 
+@login_required
 def player_create(request):
     if request.method == "POST":
         form = PlayerCreateForm(request.POST)
