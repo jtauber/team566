337b1ecc9f0954717256e8c11acf7ec8a2f74a03
Brian Rosner <brosner@gmail.com>
2010-08-14 15:35:59 -0600

    added missing import

--- manoria_project/apps/manoria/forms.py
+++ manoria_project/apps/manoria/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from manoria.models import Player, Settlement, SettlementBuilding
+from manoria.models import Player, Settlement, SettlementBuilding, SettlementTerrain
 
 
 class PlayerCreateForm(forms.ModelForm):
